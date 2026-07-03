eaa7e815352f8ece95a1f76982a22d4f4cc3b9e1
jungpill <ljp10777@gmail.com>
2026-07-04 00:52:31 +0900

    baseurl변경

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "https://jungpilportfolio.shop/",
+    baseURL: "https://api.jungpilportfolio.shop",
     headers: {
       'X-Custom-Header': 'foobar',
       'Content-Type': 'application/json',
